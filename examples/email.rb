aa1f1b4c38e3c4e7debc41b1f500bf8ee05bb5bd
Alex MacCaw <info@eribium.org>
2014-10-22 09:07:33 -0700

    bump version

--- examples/email.rb
+++ examples/email.rb
@@ -1,6 +1,6 @@
 require 'clearbit'
 require 'pp'
 
-Clearbit.api_key = ENV['CLEARBIT_KEY']
+Clearbit.api_key = ENV['CLEARBIT_KEY_LOCAL']
 
 pp Clearbit::Person[email: 'info@eribium.org']
--- lib/clearbit/version.rb
+++ lib/clearbit/version.rb
@@ -1,3 +1,3 @@
 module Clearbit
-  VERSION = "0.0.7"
+  VERSION = "0.0.8"
 end
