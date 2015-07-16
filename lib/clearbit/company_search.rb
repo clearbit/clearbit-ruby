754d84073802002e3b52076a9aa185c6d36ac6e5
Alex MacCaw <alex@alexmaccaw.com>
2015-07-16 10:54:40 -0700

    add pagination

--- lib/clearbit/company_search.rb
+++ lib/clearbit/company_search.rb
@@ -3,6 +3,37 @@ module Clearbit
     endpoint 'https://company.clearbit.com'
     path '/v1/companies/search'
 
+    class PagedResult < Delegator
+      def initialize(params, response)
+        @params = params
+        super Mash.new(response)
+      end
+
+      def __getobj__
+        @response
+      end
+
+      def __setobj__(obj)
+        @response = obj
+      end
+
+      def each(&block)
+        return enum_for(:each) unless block_given?
+
+        results.each do |result|
+          yield result
+        end
+
+        if results.any?
+          search = CompanySearch.search(
+            @params[:query],
+            @params.merge(page: page + 1)
+          )
+          search.each(&block)
+        end
+      end
+    end
+
     def self.search(query, params = {})
       options  = params.delete(:request) || {}
       options  = options.merge(format: :json)
@@ -12,7 +43,7 @@ module Clearbit
 
       response = post('', params, options)
 
-      self.new(response)
+      PagedResult.new(params, response)
     end
   end
 end
