b54f529e7603f75d01ecf683585b35f9798cd7de
Alex MacCaw <alex@alexmaccaw.com>
2015-09-25 12:00:16 -0700

    Update Prospector API

--- lib/clearbit/prospector.rb
+++ lib/clearbit/prospector.rb
@@ -6,5 +6,22 @@ module Clearbit
     def self.search(values = {})
       self.new get('search', values)
     end
+
+    def email
+      email_response.email
+    end
+
+    def verified?
+      email_response.verified
+    end
+
+    protected
+
+    def email_response
+      @email_response ||= begin
+        response = self.class.get(uri(:email))
+        Mash.new(response.decoded)
+      end
+    end
   end
 end
