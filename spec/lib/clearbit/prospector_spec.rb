03d80d6bba531602d8f0fafa9527b3481678fb48
Tristan Dunn <tristanzdunn@gmail.com>
2018-11-15 22:25:21 +0100

    Bump the Prospector path version. (#29)
    * Bump the Prospector path version.
    
    Also add and updates a couple of tests.
    
    * Update the Prospector example script.
    
    * Add page parameter to the Prospector example.
    
    * bump major version
    
    * back to v1
    
    * bump not needed

--- spec/lib/clearbit/prospector_spec.rb
+++ spec/lib/clearbit/prospector_spec.rb
@@ -6,14 +6,24 @@ describe Clearbit::Prospector do
   end
 
   context 'Prospector API' do
-    it 'should call out to the Prospector API' do
-      body  = []
+    it 'calls the Prospector API' do
+      body = { page: 1, page_size: 5, total: 723, results: [] }
 
       stub_request(:get, "https://prospector.clearbit.com/v1/people/search?domain=stripe.com").
         with(:headers => {'Authorization'=>'Bearer clearbit_key'}).
         to_return(:status => 200, :body => body.to_json, headers: {'Content-Type' => 'application/json'})
 
       Clearbit::Prospector.search(domain: 'stripe.com')
+    end
+
+    it 'can page through records' do
+      body = { page: 2, page_size: 10, total: 12, results: [] }
+
+      stub_request(:get, "https://prospector.clearbit.com/v1/people/search?domain=stripe.com&page=2&page_size=10").
+        with(:headers => {'Authorization'=>'Bearer clearbit_key'}).
+        to_return(:status => 200, :body => body.to_json, headers: {'Content-Type' => 'application/json'})
+
+      Clearbit::Prospector.search(domain: 'stripe.com', page: 2, page_size: 10)
     end
   end
 end
