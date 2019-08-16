813d36f47eeeebb0c3dfa650e4ac77d5a7c93818
David Lumley <david@davidlumley.xyz>
2019-08-16 14:33:59 -0700

    add `.page` method to Clearbit::Analytics

--- spec/lib/clearbit/analytics_spec.rb
+++ spec/lib/clearbit/analytics_spec.rb
@@ -6,20 +6,41 @@ describe Clearbit::Analytics do
     Clearbit.key = 'clearbit_key'
   end
 
-  it 'sends an identify request to Clearbit X' do
-    basic_auth = Base64.encode64('clearbit_key:').strip
-    x_stub = stub_request(:post, 'https://x.clearbit.com/v1/import').
-      with(
-        headers: { 'Authorization' => "Basic #{basic_auth}" }
-      ).to_return(status: 200, body: {success: true}.to_json)
+  describe '#identify' do
+    it 'sends an identify request to Clearbit X' do
+      basic_auth = Base64.encode64('clearbit_key:').strip
+      x_stub = stub_request(:post, 'https://x.clearbit.com/v1/import').
+        with(
+          headers: { 'Authorization' => "Basic #{basic_auth}" }
+        ).to_return(status: 200, body: {success: true}.to_json)
 
-    Clearbit::Analytics.identify(
-      user_id: '123',
-      traits: {
-        email: 'david@clearbit.com',
-      },
-    )
+      Clearbit::Analytics.identify(
+        user_id: '123',
+        traits: {
+          email: 'david@clearbit.com',
+        },
+      )
 
-    expect(x_stub).to have_been_requested
+      expect(x_stub).to have_been_requested
+    end
+  end
+
+  describe '#page' do
+    it 'sends an identify request to Clearbit X' do
+      basic_auth = Base64.encode64('clearbit_key:').strip
+      x_stub = stub_request(:post, 'https://x.clearbit.com/v1/import').
+        with(
+          headers: { 'Authorization' => "Basic #{basic_auth}" }
+        ).to_return(status: 200, body: {success: true}.to_json)
+
+      Clearbit::Analytics.page(
+        user_id: '123',
+        traits: {
+          email: 'david@clearbit.com',
+        },
+      )
+
+      expect(x_stub).to have_been_requested
+    end
   end
 end
