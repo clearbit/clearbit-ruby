6f3d17d4d87b22d1365aa77bc8511c17ab76f48e
David Lumley <david@davidlumley.xyz>
2020-02-18 10:57:45 -0800

    add group event to analytics (#36)
    * add group method
    
    * add specs
    
    * bump version

--- lib/clearbit/version.rb
+++ lib/clearbit/version.rb
@@ -1,3 +1,3 @@
 module Clearbit
-  VERSION = '0.3.1'
+  VERSION = '0.3.2'
 end
--- spec/lib/clearbit/analytics_spec.rb
+++ spec/lib/clearbit/analytics_spec.rb
@@ -43,4 +43,24 @@ describe Clearbit::Analytics do
       expect(x_stub).to have_been_requested
     end
   end
+
+  describe '#group' do
+    it 'sends an identify request to Clearbit X' do
+      basic_auth = Base64.encode64('clearbit_key:').strip
+      x_stub = stub_request(:post, 'https://x.clearbit.com/v1/import').
+        with(
+          headers: { 'Authorization' => "Basic #{basic_auth}" }
+        ).to_return(status: 200, body: {success: true}.to_json)
+
+      Clearbit::Analytics.group(
+        user_id: '123',
+        group_id: '123',
+        traits: {
+          domain: 'clearbit.com',
+        },
+      )
+
+      expect(x_stub).to have_been_requested
+    end
+  end
 end
